329288f920c403cc3ab40ddd06a332ccbe774fde
Kamil Niezrecki <kamil.niezrecki@gmail.com>
2016-05-09 23:49:14 +0200

    Remove

--- softblue/SoftBlueUI/app/src/main/java/com/softblue/app/utils/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package com.softblue.app.utils;
-
-/**
- * Created by kamil.niezrecki@gmail.com on 09.05.2016.
- */
-public class StringUtils {
-    public static final String SPACE = " ";
-    public static final String DASH = " - ";
-}
