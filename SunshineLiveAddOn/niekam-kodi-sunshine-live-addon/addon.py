6b9a04a98a5ac205602cced5171a5ea78382b00e
Kamil Niezrecki <kamil.niezrecki@gmail.com>
2017-12-04 23:08:08 +0100

    Add new stations

--- SunshineLiveAddOn/niekam-kodi-sunshine-live-addon/addon.py
+++ SunshineLiveAddOn/niekam-kodi-sunshine-live-addon/addon.py
@@ -11,7 +11,7 @@ import xbmcplugin
 ptv = xbmcaddon.Addon()
 BASE_RESOURCE_PATH = os.path.join(ptv.getAddonInfo('path'), "resources")
 sys.path.append(os.path.join(BASE_RESOURCE_PATH, "lib"))
-jsonData = os.path.join(ptv.getAddonInfo('path'), "resources") + os.path.sep + "radios.json"
+jsonData = os.path.join(ptv.getAddonInfo('path'), "resources") + os.path.sep + "config.json"
 fanart = os.path.join(ptv.getAddonInfo('path'), 'fanart.jpg')
 
 def build_url(query):
@@ -28,19 +28,19 @@ def get_metadata(url):
         info['title'] = data['title']
     if 'artist' in data:
         info['artist'] = data['artist']
-    
+
     return info
-        
+
 def build_song_list():
     song_list = []
     json_data = open(jsonData, "r")
     data = simplejson.loads(json_data.read())
     json_data.close()
-    thumbnail_url = data['thumbnail_url']
-    metadata_url = data['metadata_url']
-    for song in data["songs"]:
+    thumbnail_url = data['thumbnail_base_url']
+    metadata_url = data['metadata_base_url']
+    for song in data["stations"]:
         info = get_metadata(metadata_url + song['metadata'])
-        
+
         li = xbmcgui.ListItem(label=song['name'], thumbnailImage=get_thumbnail_url(thumbnail_url, song['thumbnail']))
         li.setProperty('IsPlayable', 'true')
         li.setProperty('fanart_image', fanart)
@@ -48,18 +48,18 @@ def build_song_list():
         url = build_url({'mode': 'stream', 'url': song['url'], 'title': song['name']})
         # add the current list item to a list
         song_list.append((url, li, False))
-    
+
     xbmcplugin.addDirectoryItems(addon_handle, song_list, len(song_list))
     # set the content of the directory
     xbmcplugin.setContent(addon_handle, 'songs')
     xbmcplugin.endOfDirectory(addon_handle)
-    
+
 def play_song(url):
     # set the path of the song to a list item
     play_item = xbmcgui.ListItem(path=url)
     # the list item is ready to be played by Kodi
     xbmcplugin.setResolvedUrl(addon_handle, True, listitem=play_item)
-    
+
 def main():
     args = urlparse.parse_qs(sys.argv[2][1:])
     mode = args.get('mode', None)
@@ -71,7 +71,7 @@ def main():
     elif mode[0] == 'stream':
         # pass the url of the song to play_song
         play_song(args['url'][0])
-    
+
 if __name__ == '__main__':
     addon_handle = int(sys.argv[1])
     main()
